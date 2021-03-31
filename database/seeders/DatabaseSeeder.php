785958f9d06fc4d55f16869707e0b59a968fa59d
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-03-30 23:45:34 -0600

    Configuracion inicial

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Note;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'email' => 'i@admin.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        Note::factory(30)->create();
     }
 }
